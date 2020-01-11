061e57f9968ddad7ae9f735c2fe814ad47f68146
Greco380 <jdg1492@rit.edu>
2020-01-11 18:13:38 -0500

    the first feature is now working

--- part1.py
+++ part1.py
@@ -1,3 +1,4 @@
+import random
 from random import randint
 # This program is to make a 5 by 5 2D array
 # This is going to be the format for my code names game
@@ -20,7 +21,7 @@ def speaker_playmat():
 
 def player_who_starts():
     start = randint(0, 1)
-    if start == 1:
+    if start == 0:
         print("Blue")
     else:
         print("Red")
@@ -36,14 +37,75 @@ def random_value(arr, starter):
     return arr
 
 
+# def random_placement(arr, starter):
+#     if starter == 1:
+#         for i in range(0, 9):
+#             x = randint(0, 4)
+#             y = randint(0, 4)
+#             arr[x][y] = 3
+#         for row in arr:
+#             print(row)
+#         return arr
+#     else:
+#         for i in range(0, 8):
+#             x = randint(0, 4)
+#             y = randint(0, 4)
+#             arr[x][y] = 3
+#         for row in arr:
+#             print(row)
+#         return arr
+
+
+def list_of_possibilities(starter):
+    array = list()
+    start_max = 9
+    second_max = 8
+    spy = 1
+    bystander = 7
+    if starter == 0:
+        for i in range(0, start_max):
+            array += ["B"]
+        for i in range(0, second_max):
+            array += ["R"]
+        for i in range(0, spy):
+            array += ["spy"]
+        for i in range(0, bystander):
+            array += ["By"]
+    else:
+        for i in range(0, start_max):
+            array += ["R"]
+        for i in range(0, second_max):
+            array += ["B"]
+        for i in range(0, spy):
+            array += ["spy"]
+        for i in range(0, bystander):
+            array += ["By"]
+    return array
+
+
+def make_array(arr, list):
+    for i in range(0, 5):
+        for j in range(0, 5):
+            # element = random.choice(list)
+            rint = randint(0, len(list) - 1)
+            element = list.pop(rint)
+            arr[i][j] = element
+            # list = list.remove(element)
+    for row in arr:
+        print(row)
+    return arr
+
 
 
 def main():
     arr = speaker_playmat()
     print("\nWho starts:")
     starter = player_who_starts()
-    print("\nRandom num grid:")
-    random_value(arr, starter)
+    # print("\nRandom num grid:")
+    # random_value(arr, starter)
+    print("\n---------------")
+    list = list_of_possibilities(starter)
+    make_array(arr, list)
 
 
 
