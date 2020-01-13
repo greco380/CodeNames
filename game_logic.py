b239c5140a28eb69481ba7724945b8901692f089
Greco380 <jdg1492@rit.edu>
2020-01-13 01:00:03 -0500

    I got most of the game logic done!!!

--- game_logic.py
+++ game_logic.py
@@ -12,11 +12,31 @@ def link_key_with_board():
             # dict name[key] = assign
             link[board[i][j]] = key[i][j]
         link[key[i][j]] = board[i][j]
-    print(link)
+    return link
 
 
-def second():
-    pass
+# def first():
+#     hint = str(input("Enter your one-word hint: "))
+#     number = int(input("Enter how many cards the hint is for: "))
+#     return hint, number
+
+
+def second(number, linked_dict):
+    for i in range(0, number + 1):
+        guess = str(input("Enter your guess( None, ?, and enter ends your turn): "))
+        if guess in linked_dict:
+            # return linked_dict.get(guess)  # need to not return here because I want to stay in the for loop
+            get_value = linked_dict.get(guess)
+            if get_value == 'Bystander':
+                print(get_value)
+                return None
+            else:
+                print(get_value)
+        elif guess == "None" or "?" or "":
+            return None
+        else:
+            print("That word is not on the board")
+            return None
 
 
 def main():
@@ -30,7 +50,13 @@ def main():
         print(row)
     print("\n-------------------")
     print("The dictionary that links the board to the key")
-    link_key_with_board()
+    linked_dict = link_key_with_board()
+    print(linked_dict)
+    print("\n\n")
+    for i in range(1, 15):
+        str(input("Enter your one-word hint: "))
+        number = int(input("Enter how many cards the hint is for: "))
+        print(second(number, linked_dict))
 
 
 if __name__ == '__main__':
